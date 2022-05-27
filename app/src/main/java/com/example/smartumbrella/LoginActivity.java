13ffeb8850e292b874b79eacfbfe60d76155714c
Mattsnows2 <matthias.belot54@gmail.com>
2022-05-27 11:29:59 +0200

    splash screen and design

--- app/src/main/java/com/example/smartumbrella/LoginActivity.java
+++ app/src/main/java/com/example/smartumbrella/LoginActivity.java
@@ -1,15 +1,24 @@
 package com.example.smartumbrella;
 
+import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,10 +27,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.concurrent.Executor;
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText editTextEmail, editTextPassword;
+    ImageView imageFigerPrint;
     Button buttonSingin;
+    private static final  int REQUEST_CODE=101010;
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +48,29 @@ public class LoginActivity extends AppCompatActivity {
         editTextEmail=findViewById(R.id.editTextLoginEmail);
         editTextPassword=findViewById(R.id.editTextLoginPassword);
         buttonSingin=findViewById(R.id.buttonSignin);
+        imageFigerPrint=findViewById(R.id.imageFingerPrint);
+
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.d("MY_APP_TAG", "App can authenticate using biometrics.");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this, "FingerPrint doest not exist", Toast.LENGTH_SHORT).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this, "Sensor not evail or busy ", Toast.LENGTH_SHORT).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                // Prompts the user to create credentials that your app accepts.
+                final Intent enrollIntent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+                enrollIntent.putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                        BIOMETRIC_STRONG | DEVICE_CREDENTIAL);
+                startActivityForResult(enrollIntent, REQUEST_CODE);
+                break;
+        }
+
+
 
         buttonSingin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +78,48 @@ public class LoginActivity extends AppCompatActivity {
                 signIn(editTextEmail.getText().toString(), editTextPassword.getText().toString());
             }
         });
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(LoginActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Intent intent=new Intent(LoginActivity.this, SplashScreen.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Use account password")
+                .build();
+
+
+        imageFigerPrint.setOnClickListener(view -> {
+            biometricPrompt.authenticate(promptInfo);
+        });
     }
+
 
     public void signIn(String email, String password){
         mAuth.signInWithEmailAndPassword(email, password)
---
+++ app/src/main/java/com/example/smartumbrella/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.example.smartumbrella;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.icu.lang.UCharacter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView umbrella, goutte1,goutte2,goutte3,goutte4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        topAnim= AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        bottomAnim= AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+        umbrella=findViewById(R.id.imageUmbrella2);
+        goutte1=findViewById(R.id.imageGoutte1);
+        goutte2=findViewById(R.id.imageGoutte2);
+        goutte3=findViewById(R.id.imageGoutte3);
+        goutte4=findViewById(R.id.imageGoutte4);
+
+        umbrella.setAnimation(topAnim);
+        goutte1.setAnimation(bottomAnim);
+        goutte2.setAnimation(bottomAnim);
+        goutte3.setAnimation(bottomAnim);
+        goutte4.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(()->{
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+
+            finish();
+        },3000);
+    }
+}
