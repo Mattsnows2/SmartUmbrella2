2bf658b3f0a4e40a12a76bfc884f00fd0d1cdd34
matus <matuscactus@gmail.com>
2022-05-28 21:01:59 +0200

    miscellaneous fixes 3

--- app/src/main/java/com/example/smartumbrella/AccountActivity.java
+++ app/src/main/java/com/example/smartumbrella/AccountActivity.java
@@ -2,9 +2,11 @@ package com.example.smartumbrella;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,19 +36,19 @@ public class AccountActivity extends AppCompatActivity {
         mBuilder.setSingleChoiceItems(listItems, -1, (dialog, which) -> {
             if(which==0){
                 changeLanguage("en");
-                recreate();
+                restartActivity();
             }else  if(which==1){
                 changeLanguage("da");
-                recreate();
+                restartActivity();
             }else  if(which==2){
                 changeLanguage("fr");
-                recreate();
+                restartActivity();
             }else  if(which==3){
                 changeLanguage("sv");
-                recreate();
+                restartActivity();
             }else  if(which==4){
                 changeLanguage("se");
-                recreate();
+                restartActivity();
             }
 
             dialog.dismiss();
@@ -72,11 +74,28 @@ public class AccountActivity extends AppCompatActivity {
     }
 
     public void loadLocale(){
-
-
         SharedPreferences prefs =  getSharedPreferences("Settings", Activity.MODE_PRIVATE);
         String language =prefs.getString("My_Lang","");
         changeLanguage(language);
     }
+
+    private void restartActivity() {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent a = new Intent(this,UserChoice.class);
+            a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(a);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
 
